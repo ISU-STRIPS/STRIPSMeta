4bdf5cb65712b34452291e5edec5b01caa677227
Jarad Niemi <niemi@iastate.edu>
2016-10-20 10:05:14 -0500

    Add package documentation

---
+++ R/STRIPSMeta.R
@@ -0,0 +1,14 @@
+#' STRIPSMeta: A package for STRIPS meta data.
+#'
+#' The STRIPSMeta package contains STRIPS meta data including watershed
+#' information, e.g. treatment, size, etc., and crop rotation.
+#'
+#' @section STRIPSMeta data sets:
+#' \code{\link{watersheds}}
+#' \code{\link{crop_rotation}}
+#'
+#' @seealso \code{\link{watersheds}}, \code{\link{crop_rotation}}
+#'
+#' @docType package
+#' @name STRIPSMeta
+NULL
