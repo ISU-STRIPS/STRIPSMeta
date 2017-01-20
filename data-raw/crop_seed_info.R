964602a1ceadc531121da4f5cffc453e33271e56
clabuzze <clabuzze@iastate.edu>
2017-01-20 12:26:17 -0600

    Simplify sub() function by removing lapply

--- data-raw/crop_seed_info.R
+++ data-raw/crop_seed_info.R
@@ -3,6 +3,6 @@ library(tidyr)
 
 crop_seed_info <- readr::read_csv("crop_seed_info.csv")  %>%
   #Remove extraneous codes from notes column entries such as "RR2 = "
-  mutate(notes = lapply(X = notes, FUN = function(x) { sub(".*=\\s+?", '', x) }))
+  mutate(notes = sub(".*=\\s+?", '', notes))
 
 devtools::use_data(crop_seed_info, overwrite = TRUE)
