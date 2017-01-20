3f5cea03538c1d80469e9a3690ad1d9b05a88c0b
clabuzze <clabuzze@iastate.edu>
2017-01-20 12:22:02 -0600

    Add script to clean data (notes column)

--- data-raw/crop_seed_info.R
+++ data-raw/crop_seed_info.R
@@ -1,2 +1,8 @@
-crop_seed_info <- readr::read_csv("crop_seed_info.csv")
+library(dplyr)
+library(tidyr)
+
+crop_seed_info <- readr::read_csv("crop_seed_info.csv")  %>%
+  #Remove extraneous codes from notes column entries such as "RR2 = "
+  mutate(notes = lapply(X = notes, FUN = function(x) { sub(".*=\\s+?", '', x) }))
+
 devtools::use_data(crop_seed_info, overwrite = TRUE)
