6138d035d98d960c8ac14aca71cf147d7676460d
clabuzze <clabuzze@iastate.edu>
2017-01-21 13:41:27 -0600

    Update STRIPSMeta.R documentation in /R to reflect crop_rotation deletion

--- R/STRIPSMeta.R
+++ R/STRIPSMeta.R
@@ -1,14 +1,13 @@
 #' STRIPSMeta: A package for STRIPS meta data.
 #'
 #' The STRIPSMeta package contains STRIPS meta data including watershed
-#' information, e.g. treatment, size, etc., crop rotation, and crop seed details.
+#' information, e.g. treatment, size, etc., and crop seed details.
 #'
 #' @section STRIPSMeta data sets:
 #' \code{\link{watersheds}}
-#' \code{\link{crop_rotation}}
 #' \code{\link{crop_seed_info}}
 #'
-#' @seealso \code{\link{watersheds}}, \code{\link{crop_rotation}}, \code{\link{crop_seed_info}}
+#' @seealso \code{\link{watersheds}}, \code{\link{crop_seed_info}}
 #'
 #' @docType package
 #' @name STRIPSMeta
